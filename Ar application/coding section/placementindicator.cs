654211dee71fda6881f849a210a671b9ab8c80d2
BISWAJIT SAHOO <76867057+biswajit91919@users.noreply.github.com>
2022-03-02 17:52:16 +0530

    Add files via upload

---
+++ Ar application/coding section/placementindicator.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.Experimental.XR;
+
+public class placementindicator : MonoBehaviour
+{
+    private ARRaycastManager raymanager;
+    public GameObject visual;
+    objectspawnner objectspawnner;
+    public GameObject loading;
+
+    void Start()
+    {
+        raymanager = FindObjectOfType<ARRaycastManager>();
+        visual = transform.GetChild(0).gameObject;
+        objectspawnner = FindObjectOfType<objectspawnner>();
+
+        visual.SetActive(false);
+
+    }
+
+    void Update()
+    {
+
+
+        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        raymanager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.Planes);
+
+        if(hits.Count > 0)
+        {
+            transform.position = hits[0].pose.position;
+            transform.rotation = hits[0].pose.rotation;
+
+            if (!visual.activeInHierarchy)
+            {
+                visual.SetActive(true);
+                loading.SetActive(false);
+            }
+
+
+        }
+
+        if(objectspawnner.setonce == true)
+        {
+
+            visual.SetActive(false);
+
+        }
+
+
+
+
+
+    }
+}
---
+++ Ar application/coding section/sliderscalerot.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class sliderscalerot : MonoBehaviour
+{
+
+    private Slider scaleslider, rotationslider;
+
+    public float scaleminvalue, scalemaxvalue, rotminvalue, rotmaxvalue;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scaleslider = GameObject.Find("scale").GetComponent<Slider>();
+        scaleslider.maxValue = scalemaxvalue;
+        scaleslider.minValue = scaleminvalue;
+
+        scaleslider.onValueChanged.AddListener(scalesliderupdate);
+
+        rotationslider = GameObject.Find("rotate").GetComponent<Slider>();
+        rotationslider.maxValue = rotmaxvalue;
+        rotationslider.minValue = rotminvalue;
+
+        rotationslider.onValueChanged.AddListener(rotatesliderupdate);
+
+
+    }
+
+
+    void scalesliderupdate(float value)
+    {
+
+        transform.localScale = new Vector3(value, value, value);
+
+    }
+
+    void rotatesliderupdate(float value)
+    {
+        transform.localEulerAngles = new Vector3(transform.rotation.x, value, transform.rotation.z);
+    }
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
